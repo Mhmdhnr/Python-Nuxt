0f8e8d2bc645fc9cdc17cb0815cc3439f4e402e2
Mhmd <mhmd.hnr@gmail.com>
2021-11-06 16:32:55 +0330

    1

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ from services.random_names import RandomNames
 app = Flask(__name__)
 app.config.update(
     DEBUG=True,
-    SERVER_NAME='127.0.0.1:5000',
+    SERVER_NAME='flask-api.myddns.me:8080',
     SECRET_KEY='secret_xxx'
 )
 params = urllib.parse.quote_plus('DRIVER={SQL Server};SERVER=localhost;DATABASE=my-api;Trusted_Connection=yes;')
