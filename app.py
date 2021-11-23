5bab905bbafb0d625c56f8299838c9d91983f775
Mhmd <mhmd.hnr@gmail.com>
2021-11-24 01:33:03 +0330

    mbti result

--- app.py
+++ app.py
@@ -5,7 +5,6 @@ from flask_cors import CORS
 from services.random_x_y import RandomXY
 from services.random_names import RandomNames
 from services.tests import TestsServices, QuestionsServices, ChoicesServices, TestServices, RavenServices, MBTIServices
-    MBTIServices
 
 app = Flask(__name__)
 app.config.update(
@@ -16,6 +15,8 @@ app.config.update(
 )
 app.config['CORS_HEADERS'] = 'Content-Type'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://lxdcspgcawenix:795dce1c1a77b56c2582bddbf92d11296d4483e04c1e82dc8db210d4bddda7bf@ec2-3-214-121-14.compute-1.amazonaws.com:5432/d5ur0qqint8ced"
+# app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql+psycopg2://postgres:mhmdhnr232323@127.0.0.1:5432/my-api"
 
 api = Api(app)
 CORS(app, resources={r"/*": {"origins": "*"}})
